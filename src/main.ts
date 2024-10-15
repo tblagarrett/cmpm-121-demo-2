4ed433b624690217ad575262036c9b9e0a94a8a5
tblagarrett <tblagarrett@gmail.com>
2024-10-14 23:22:39 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,14 @@ const APP_NAME = "Hello";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+// Add elements
+const header = document.createElement("h1");
+header.innerHTML = APP_NAME;
+app.append(header);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+canvas.id = "drawingCanvas";
+app.appendChild(canvas);
